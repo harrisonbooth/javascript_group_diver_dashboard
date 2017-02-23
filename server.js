d0f3139827c66f218315cdbc82245371687aeb97
Harrison Booth <harrisonbooth@ymail.com>
2017-02-23 14:11:48 +0000

    working server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(require('./controllers'));
+
+app.use(express.static('client/public'));
+
+app.listen(3000, function () {
+  console.log('App running on port '+this.address().port);
+});
