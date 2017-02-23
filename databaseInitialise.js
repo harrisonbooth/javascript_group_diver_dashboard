7bbb72e2a5c74584b2fc9d023d04765a745f34e0
Harrison Booth <harrisonbooth@ymail.com>
2017-02-23 14:24:17 +0000

    add database intialiser

---
+++ databaseInitialise.js
@@ -0,0 +1,18 @@
+use journal;
+db.dropDatabase();
+
+use journal;
+db.journalEntries.insert(
+  [
+    {
+      entryNumber: 1,
+      timestamp: 'Thu Feb 23 2017 14:20:29',
+      content: 'Hello world!'
+    },
+    {
+      entryNumber: 2,
+      timestamp: 'Fri Feb 24 2098 14:30:12',
+      content: 'This mission has really dragged on, I haven\'t used this nearly enough!...'
+    }
+  ]
+);
