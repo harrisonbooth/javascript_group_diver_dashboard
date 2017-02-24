6bcfdc6ee113ba54e5c6a9ae720fe2f10d817470
Kristie Yorkston <kyorkston@gmail.com>
2017-02-24 16:00:48 +0000

    add geolocation

--- client/src/models/mapWrapper.js
+++ client/src/models/mapWrapper.js
@@ -3,7 +3,22 @@ var MapWrapper = function(coords, zoom, container){
     center: coords,
     zoom: zoom
   });
+  this.getUserLocation();
 };
+
+MapWrapper.prototype = {
+  getUserLocation: function(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(function(position){
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+        var coords = new google.maps.LatLng(lat, lng);
+        this.googleMap.setCenter(coords);
+      }.bind(this))
+    }
+  }
+
+}
 
 
 module.exports = MapWrapper;
