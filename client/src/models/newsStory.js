c48991bb4cf532113c5f7b62e5ab3214eff0e68c
Amy Robinson <amskyrobinson@gmail.com>
2017-02-26 11:50:09 +0000

    small commit news headline content scroller working

---
+++ client/src/models/newsStory.js
@@ -0,0 +1,29 @@
+var NewsStory = function(){}
+
+
+NewsStory.prototype = {
+
+  makeRequest: function(url, callback){
+      var request = new XMLHttpRequest();
+      request.open('GET', url);
+      request.onload = callback;
+      request.send();
+    },
+
+
+    newsStoryResponse: function(callback){
+      this.makeRequest("https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=50987132659b4da4bc4dd9bf9b059612", function(){
+        if(this.status !== 200) return;
+        var jsonString = this.responseText;
+        var newsResults = JSON.parse(jsonString);
+        newsArray = newsResults.articles;
+        // console.log(newsArray);
+
+        callback(newsArray);
+      });
+    }
+  }
+
+
+
+module.exports = NewsStory;
