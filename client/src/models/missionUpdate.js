e9e85be74f4fdf42e8ad4a7b4a3c06838e254985
Kristie Yorkston <kyorkston@gmail.com>
2017-02-26 14:04:22 +0000

    add mission update model

--- client/src/models/missionUpdate.js
+++ client/src/models/missionUpdate.js
@@ -3,5 +3,18 @@ var MissionUpdate = function(){}
 MissionUpdate.prototype = {
   makeRequest: function(url, callback){
     var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = callback;
+    request.send();
+  },
+
+  listofMissions: function(callback){
+    this.makeRequest("http://localhost:3000/api/mission", function(){
+      if(this.status !== 200) return;
+      var jsonString = this.responseText;
+      var missionList = JSON.parse(jsonString);
+
+      callback(missionList);
+    }
   }
 }
