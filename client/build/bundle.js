b5308a9fad1a582ce4bd3c183e4298f887ac6598
Harrison Booth <harrisonbooth@ymail.com>
2017-02-26 12:05:27 +0000

    add separate ui file for news api

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -87,17 +87,16 @@ module.exports = JournalEntry;
 var JournalEntryList = __webpack_require__(2);
 var JournalEntry = __webpack_require__(0);
 var MapWrapper = __webpack_require__(3);
-var NewsStory = __webpack_require__(5);
+var NewsUI = __webpack_require__(6);
 
 var UI = function(){
   this.entryList = new JournalEntryList();
   this.entryList.listOfEntries(function(results){
     this.populateSelect(results);
   }.bind(this));
-  this.newsStory = new NewsStory();
-  this.newsStory.newsStoryResponse(function(newsArray){
-    this.showNewsStory(newsArray);
-  }.bind(this));
+
+  var newsUI = new NewsUI();
+
   this.showMap();
 };
 
@@ -216,76 +215,7 @@ UI.prototype = {
     returnGeoLocation.id = 'user-location-button';
     mapControls.appendChild(returnGeoLocation);
     returnGeoLocation.onclick = mainMap.getUserLocation.bind(mainMap);
-  },
-
- showNewsStory: function(newsArray){
-    var ultraContainer = document.getElementById('ultra-news-story-container');
-    var container = document.getElementById('news-story-container');
-    var links = document.getElementsByClassName('itemLinks');
-    var counter = 0;
-    var activeLink = 0;
-
-    for(var i = 0; i < links.length; i++) {
-      var link = links[i];
-      link.addEventListener('click', setClickedItem, false);
-
-      link.itemID = i;
-    }
-
-    links[activeLink].classList.add("active");
-    console.log(links);
-
-
-    function setClickedItem(event) {
-        removeActiveLinks();
-     
-        var clickedLink = event.target;
-        activeLink = clickedLink.itemID;
-     
-        changePosition(clickedLink);
-    }
-
-    function removeActiveLinks() {
-        for (var i = 0; i < links.length; i++) {
-            links[i].classList.remove("active");
-        }
-    }
-
-    function changePosition(link) {
-        var position = link.getAttribute("data-pos");
-     
-        var translateValue = "translate3d(" + position + ", 0px, 0)";
-        container.style.transform = translateValue;
-     
-        link.classList.add("active");
-    }
-
-    newsArray.forEach(function(story){
-      var div = document.createElement('div');
-      div.setAttribute('className', "content");
-      var idNumber = counter;
-      counter++;
-      div.setAttribute('id', 'item' + counter);
-
-      var image = document.createElement('img');
-      var storyP = document.createElement('p');
-      var a = document.createElement('a');
-      image.src = story.urlToImage;
-      image.height = "200";
-      image.width = "220";
-      a.setAttribute('href', story.url);
-      a.innerText = story.title;
-
-      storyP.appendChild(a);
-      div.appendChild(image);
-      div.appendChild(storyP)
-      container.appendChild(div);
-      ultraContainer.appendChild(container);
-
-    });
-
- }
-
+  }
 
 }
 
@@ -455,6 +385,89 @@ NewsStory.prototype = {
 
 
 module.exports = NewsStory;
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var NewsStory = __webpack_require__(5);
+
+var NewsUI = function(){
+  this.newsStory = new NewsStory();
+  this.newsStory.newsStoryResponse(function(newsArray){
+    this.showNewsStory(newsArray);
+  }.bind(this));
+}
+
+NewsUI.prototype = {
+  showNewsStory: function(newsArray){
+    var ultraContainer = document.getElementById('ultra-news-story-container');
+    var container = document.getElementById('news-story-container');
+    var links = document.getElementsByClassName('itemLinks');
+    var counter = 0;
+    var activeLink = 0;
+
+    for(var i = 0; i < links.length; i++) {
+      var link = links[i];
+      link.addEventListener('click', setClickedItem, false);
+
+      link.itemID = i;
+    }
+
+    links[activeLink].classList.add("active");
+
+    function setClickedItem(event) {
+      removeActiveLinks();
+
+      var clickedLink = event.target;
+      activeLink = clickedLink.itemID;
+
+      changePosition(clickedLink);
+    }
+
+    function removeActiveLinks() {
+      for (var i = 0; i < links.length; i++) {
+        links[i].classList.remove("active");
+      }
+    }
+
+    function changePosition(link) {
+      var position = link.getAttribute("data-pos");
+
+      var translateValue = "translate3d(" + position + ", 0px, 0)";
+      container.style.transform = translateValue;
+
+      link.classList.add("active");
+    }
+
+    newsArray.forEach(function(story){
+      var div = document.createElement('div');
+      div.setAttribute('className', "content");
+      var idNumber = counter;
+      counter++;
+      div.setAttribute('id', 'item' + counter);
+
+      var image = document.createElement('img');
+      var storyP = document.createElement('p');
+      var a = document.createElement('a');
+      image.src = story.urlToImage;
+      image.height = "200";
+      image.width = "220";
+      a.setAttribute('href', story.url);
+      a.innerText = story.title;
+
+      storyP.appendChild(a);
+      div.appendChild(image);
+      div.appendChild(storyP)
+      container.appendChild(div);
+      ultraContainer.appendChild(container);
+
+    });
+  }
+}
+
+module.exports = NewsUI;
+
 
 /***/ })
 /******/ ]);
--- client/src/views/masterUI.js
+++ client/src/views/masterUI.js
@@ -1,17 +1,16 @@
 var JournalEntryList = require("../models/JournalEntryList");
 var JournalEntry = require("../models/journalEntry");
 var MapWrapper = require("../models/mapWrapper");
-var NewsStory = require("../models/newsStory");
+var NewsUI = require("./newsUI");
 
 var UI = function(){
   this.entryList = new JournalEntryList();
   this.entryList.listOfEntries(function(results){
     this.populateSelect(results);
   }.bind(this));
-  this.newsStory = new NewsStory();
-  this.newsStory.newsStoryResponse(function(newsArray){
-    this.showNewsStory(newsArray);
-  }.bind(this));
+
+  var newsUI = new NewsUI();
+
   this.showMap();
 };
 
@@ -130,76 +129,7 @@ UI.prototype = {
     returnGeoLocation.id = 'user-location-button';
     mapControls.appendChild(returnGeoLocation);
     returnGeoLocation.onclick = mainMap.getUserLocation.bind(mainMap);
-  },
-
- showNewsStory: function(newsArray){
-    var ultraContainer = document.getElementById('ultra-news-story-container');
-    var container = document.getElementById('news-story-container');
-    var links = document.getElementsByClassName('itemLinks');
-    var counter = 0;
-    var activeLink = 0;
-
-    for(var i = 0; i < links.length; i++) {
-      var link = links[i];
-      link.addEventListener('click', setClickedItem, false);
-
-      link.itemID = i;
-    }
-
-    links[activeLink].classList.add("active");
-    console.log(links);
-
-
-    function setClickedItem(event) {
-        removeActiveLinks();
-     
-        var clickedLink = event.target;
-        activeLink = clickedLink.itemID;
-     
-        changePosition(clickedLink);
-    }
-
-    function removeActiveLinks() {
-        for (var i = 0; i < links.length; i++) {
-            links[i].classList.remove("active");
-        }
-    }
-
-    function changePosition(link) {
-        var position = link.getAttribute("data-pos");
-     
-        var translateValue = "translate3d(" + position + ", 0px, 0)";
-        container.style.transform = translateValue;
-     
-        link.classList.add("active");
-    }
-
-    newsArray.forEach(function(story){
-      var div = document.createElement('div');
-      div.setAttribute('className', "content");
-      var idNumber = counter;
-      counter++;
-      div.setAttribute('id', 'item' + counter);
-
-      var image = document.createElement('img');
-      var storyP = document.createElement('p');
-      var a = document.createElement('a');
-      image.src = story.urlToImage;
-      image.height = "200";
-      image.width = "220";
-      a.setAttribute('href', story.url);
-      a.innerText = story.title;
-
-      storyP.appendChild(a);
-      div.appendChild(image);
-      div.appendChild(storyP)
-      container.appendChild(div);
-      ultraContainer.appendChild(container);
-
-    });
-
- }
-
+  }
 
 }
 
