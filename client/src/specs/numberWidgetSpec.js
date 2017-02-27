3e5b1e62e6854d2fe58ed2f82fb50bb9fda153fa
Kristie Yorkston <kyorkston@gmail.com>
2017-02-27 11:01:29 +0000

    add frame for widget spec

---
+++ client/src/specs/numberWidgetSpec.js
@@ -0,0 +1,10 @@
+var NumberWidget = require('../models/numberWidget');
+var assert = require('assert');
+
+describe('Number Widget', function(){
+
+  context('it should have constructors', function(){
+    
+  })
+
+})
