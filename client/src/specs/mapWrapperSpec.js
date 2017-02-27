d059a63aa67723fca10705a9e246789f37741f35
Kristie Yorkston <kyorkston@gmail.com>
2017-02-27 11:04:58 +0000

    add frame to map wrapper spec

---
+++ client/src/specs/mapWrapperSpec.js
@@ -0,0 +1,10 @@
+var MapWrapper = require('../models/mapWrapper');
+var assert = require('assert');
+
+describe('Map Wrapper', function(){
+
+  context('it should have constructors', function(){
+    
+  })
+
+})
