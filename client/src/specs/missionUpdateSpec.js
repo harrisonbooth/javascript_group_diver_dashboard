71abcfb62b6502c7089da7072f4183b4b0a683fd
Kristie Yorkston <kyorkston@gmail.com>
2017-02-27 10:58:25 +0000

    add frame for new test:

---
+++ client/src/specs/missionUpdateSpec.js
@@ -0,0 +1,10 @@
+var MissionUpdate = require('../models/missionUpdate');
+var assert = require('assert');
+
+describe('Mission Update', function(){
+
+  context('it should have constructors', function(){
+    
+  })
+
+})
