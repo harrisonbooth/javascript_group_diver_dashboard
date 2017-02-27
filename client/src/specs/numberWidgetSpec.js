78378b8e3ceb38b2423148e03e048c76ccb1f4ab
Kristie Yorkston <kyorkston@gmail.com>
2017-02-27 11:03:14 +0000

    add number widget spec

--- client/src/specs/numberWidgetSpec.js
+++ client/src/specs/numberWidgetSpec.js
@@ -4,7 +4,17 @@ var assert = require('assert');
 describe('Number Widget', function(){
 
   context('it should have constructors', function(){
-    
-  })
 
-})
+    var widget = new NumberWidget(3);
+
+    it('should not be null', function(){
+      assert.notEqual(null, widget);
+    });
+
+    it('should have a limit', function(){
+      assert.equal(3, widget.limit);
+    });
+
+  });
+
+});
