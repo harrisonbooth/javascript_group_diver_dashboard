111402ca538d9df29a50a8796574ffbd4a2d94fd
Kristie Yorkston <kyorkston@gmail.com>
2017-02-27 14:28:29 +0000

    add night mode to google map

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -330,11 +330,7 @@ module.exports = UI;
 /* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-var JournalEntry = __webpack_require__(0); 
-=======
 var JournalEntry = __webpack_require__(0);
->>>>>>> c3dc6b8fae2e7efa215901ee56f89d278be05ca9
 
 var JournalEntryList = function(){}
 
@@ -514,8 +510,88 @@ module.exports = NumberWidget;
 var MapWrapper = function(coords, zoom, container){
   this.googleMap = new google.maps.Map(container, {
     center: coords,
-    zoom: zoom
-  });
+    zoom: zoom,
+    styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+        });
 };
 
 MapWrapper.prototype = {
@@ -538,7 +614,7 @@ MapWrapper.prototype = {
      icon: "http://i.imgur.com/xsK9snx.png"
    })
  }
- 
+
 }
 
 
@@ -725,15 +801,9 @@ module.exports = NewsUI;
 /***/ }),
 /* 8 */
 /***/ (function(module, exports, __webpack_require__) {
-<<<<<<< HEAD
 
 var UI = __webpack_require__(1);
 
-=======
-
-var UI = __webpack_require__(1);
-
->>>>>>> c3dc6b8fae2e7efa215901ee56f89d278be05ca9
 var app = function(){
   var ui = new UI();
   var select = document.getElementById('entry-select');
