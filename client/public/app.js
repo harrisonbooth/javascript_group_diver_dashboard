8ae268837f9a717f05a7dce0c58255ad2f0882a0
Camden Lohman <camden.lohman1@gmail.com>
2017-02-28 08:35:37 +0000

    minesweeper made

--- client/public/app.js
+++ client/public/app.js
@@ -1,12 +1,19 @@
 var UI = require('../src/views/masterUI');
+var Game = require('./game_File');
+
 
 var app = function(){
+  
   var ui = new UI();
   var select = document.getElementById('entry-select');
   select.onchange = ui.selectEntry.bind(ui);
 
   var button = document.getElementById('add-new-entry');
   button.onclick = ui.newEntryForm.bind(ui);
+
+  var game = new Game();
+
 };
+
 
 window.onload = app;
