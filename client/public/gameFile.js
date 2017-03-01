f020cdd1d23e7949fe085bc36e8e45f912fa141c
Kristie Yorkston <kyorkston@gmail.com>
2017-03-01 10:05:54 +0000

    refactor game file

--- client/public/gameFile.js
+++ client/public/gameFile.js
@@ -34,7 +34,7 @@ var tileMap = [[1,1,1,1,1,1,1,1],
 [1,1,1,1,1,1,1,1]]
 
 function startGame() {
-  var myGamePiece; // what is the mine square called when made
+  var myGamePiece;
 
   //START initialize for loop (this actually makes the grid)
   for(var j = 0; j < gridHeight; j++){
@@ -56,7 +56,6 @@ while (minesCounter !== 0){
     minesCounter -= 1;
   }
 }
-
  myGameArea.start();
 }
 
@@ -65,18 +64,19 @@ var myGameArea = {
   start : function() {
     this.canvas.width = (titleWidth * gridWidth + (titleGap * (gridWidth - 1)));
     this.canvas.height = (titleHeight * gridHeight + (titleGap * (gridHeight - 1)));
-    this.context = this.canvas.getContext("2d"); 
+    this.context = this.canvas.getContext("2d");
     this.canvas.id = "canvas";
     document.getElementById("game-container").appendChild(this.canvas);
-
     this.frameNo = 0;
     this.interval = setInterval(updateGameArea, 20);
-    window.addEventListener('click', function (e) { // when click, do something
-      console.log(e);
+
+    window.addEventListener('click', function (e) {
       if (alive === true){
         for(var i = 0; i < mineArray.length; i++){
+
           if(e.clientX -1000 > mineArray[i].x + 7 && e.clientX -1000 < (mineArray[i].x + mineArray[i].width+ 9)){
             if(e.clientY -370 > mineArray[i].y + 8 && e.clientY -370 < (mineArray[i].y + mineArray[i].height + 10)){
+
               if (mineArray[i].hasMine === true){
                 mineArray[i].img.src = "./images/rsz_mine_original.png";
                 alive = false;
@@ -90,13 +90,10 @@ var myGameArea = {
             }
           }
         }
-      } else {
-        //show clickable buttons
-      }
+      } else { }
     })
-    document.onkeydown = function (e) { // when click, do something
+    document.onkeydown = function (e) {
       var currentKey = e.keyCode;
-      console.log(currentKey);
       if(currentKey === 192){
         for(var i = 0;i < mineArray.length; i++){
           mineArray[i].clicked = false;
@@ -106,11 +103,11 @@ var myGameArea = {
           mineArray[i].hasMine = false;
           alive = true;
           minesCounter = mineTotal;
-
-
         }
+
         while (minesCounter !== 0){
           randomNum = Math.floor((Math.random() * (gridWidth * gridHeight)));
+
           if (mineArray[randomNum].hasMine === true){
 
           } else{
@@ -121,14 +118,16 @@ var myGameArea = {
       }
     }
   },
+
   stop : function() {
     clearInterval(this.interval);
   },
-  clear : function() {
 
+  clear : function() {
     var ctx = this.canvas.getContext('2d');
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     ctx.globalCompositeOperation = 'lighter';
+
     for(var i=0; i<particles.length; i++) {
       var p = particles[i];
       ctx.beginPath();
@@ -136,6 +135,7 @@ var myGameArea = {
       ctx.arc(p.x, p.y, p.radius, 0, Math.PI*2, false);
       ctx.fill();
       p.y -= p.speed;
+
       if(p.y <= -10)
         particles[i] = new particle();
     }
@@ -143,10 +143,10 @@ var myGameArea = {
 }
 
 function component(width, height, x, y,x_in_grid,y_in_grid) {
-  this.width = width; // tile width
-  this.height = height; // tile height
-  this.x = x; // tile x
-  this.y = y; // tile y
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
   this.x_in_grid = x_in_grid;
   this.y_in_grid = y_in_grid;
   this.clicked = false;
@@ -154,7 +154,8 @@ function component(width, height, x, y,x_in_grid,y_in_grid) {
   this.img = new Image(this.x,this.y,30,30);
   this.img.src = "./images/default.png";
   this.changeSource = false;
-  this.hasMine = false; // does tile have a mine
+  this.hasMine = false;
+
   this.update = function(new_col) {
     if (this.clicked === false){
       ctx = myGameArea.context;
@@ -163,10 +164,10 @@ function component(width, height, x, y,x_in_grid,y_in_grid) {
       ctx.drawImage(this.img,0,0);
       ctx.restore();
     }
+
     if (this.clicked === true){
       if(this.changeSource === true){
         switch(this.close_mines){
-
           case 0:
           this.img.src = "./images/0.png";
           this.changeSource = false;
@@ -216,50 +217,57 @@ function component(width, height, x, y,x_in_grid,y_in_grid) {
 
 function checkAround(num){
   mineCheckCounter = 0;
+
   if (mineArray[num].x_in_grid !== 0){
+
     if (mineArray[num].y_in_grid !== 0){
-    //look UL
+
     if (mineArray[num-9].hasMine === true){
       mineCheckCounter += 1;
     }
   }
-    //look L
+
     if (mineArray[num-1].hasMine === true){
       mineCheckCounter += 1;
     }
+
     if (mineArray[num].y_in_grid !== gridHeight - 1){
-    //look DL
+
     if (mineArray[num+7].hasMine === true){
       mineCheckCounter += 1;
     }
   }
 }
 if (mineArray[num].y_in_grid !== 0){
-    //look U
+
     if (mineArray[num-8].hasMine === true){
       mineCheckCounter += 1;
     }
+
     if (mineArray[num].x_in_grid !== gridWidth - 1){
-    //look UR
+
     if (mineArray[num-7].hasMine === true){
       mineCheckCounter += 1;
     }
   }
 }
+
 if (mineArray[num].x_in_grid !== gridWidth - 1){
-    //look R
+
     if (mineArray[num+1].hasMine === true){
       mineCheckCounter += 1;
     }
+
     if (mineArray[num].y_in_grid !== gridHeight - 1){
-    //look DR
+
     if (mineArray[num+9].hasMine === true){
       mineCheckCounter += 1;
     }
   }
 }
+
 if (mineArray[num].y_in_grid !== gridHeight - 1){
-    //look D
+
     if (mineArray[num+8].hasMine === true){
       mineCheckCounter += 1;
     }
@@ -273,7 +281,6 @@ for(var i=0; i<particleCount;i++)
   particles.push(new particle());
 
 function particle() {
-
   this.x = Math.random() * (titleWidth * gridWidth + (titleGap * (gridWidth - 1)));
   this.y = (titleHeight * gridHeight + (titleGap * (gridHeight - 1))) + Math.random() * 300;
   this.speed = 2 + Math.random() * 5;
@@ -287,15 +294,14 @@ function loop() {
 }
 
 function updateGameArea() {
-
-  //VVVV Clears the canvas
   myGameArea.clear();
-  //VVVV Updates the grid and re-draws on canvas
+
   for(var i = 0; i < mineArray.length; i++){
     mineArray[i].update();
   }
-  if (alive === true){}
-  else {
+
+  if (alive === true){
+  } else {
     if (endDisplayOnce === false){
       console.log("you hit a mine, return to the sub");
       endDisplayOnce = true;
